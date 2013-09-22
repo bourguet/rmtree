cfdf730b6258376265ae491a5c21fb2b700539e3
Jean-Marc Bourguet <jm@bourguet.org>
2013-09-22 15:29:06 +0200

    Arguments checking

--- src/rmtree.cpp
+++ src/rmtree.cpp
@@ -1,22 +1,103 @@
 // -*- C++ -*-
 
+#include <errno.h>
 #include <exception>
 #include <iostream>
+#include <set>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+// ----------------------------------------------------------------------------
 /// Display simple usage information
 void usage()
 {
-    std::cout << "Usage: rmtree\n";
-}
+    std::cout << "Usage: rmtree [-fc] dirs...\n";
+} // usage
 
+// ----------------------------------------------------------------------------
 /// Display a full help message
 void help()
 {
     usage();
-}
+    std::cout <<
+        "Remove directories and their content\n"
+        "\n"
+        "Options:\n"
+        "-f\tforce (change permissions on removed entries, remove read only entries)\n"
+        "-c\tremove only the content, keep the directory\n";
+} // help
 
+// ----------------------------------------------------------------------------
+/// check if a path can be removed.
+/// Must exists.  Don't remove anything from .. to /.
+/// Remove . and mount point only if contentOnly.
+/// path must be a directory if contentOnly.
+bool isValidForRemoval(std::string const& path, bool contentOnly)
+{
+    bool result = true;
+    static std::set<std::pair<dev_t, ino_t> > pathToRoot;
+    static std::pair<dev_t, ino_t> cwd;
+    if (pathToRoot.empty()) {
+        struct stat buf;
+        std::string cur = ".";
+        if (lstat(cur.c_str(), &buf) != 0) {
+            int error = errno;
+            std::cerr << "Unable to stat " << cur << ": "
+                      << strerror(error) << '\n';
+            result = false;
+        }
+        cwd = std::make_pair(buf.st_dev, buf.st_ino);
+        do {
+            cur += "/..";
+            if (lstat(cur.c_str(), &buf) != 0) {
+                int error = errno;
+                std::cerr << "Unable to stat " << cur << ": "
+                          << strerror(error) << '\n';
+                result = false;
+            }
+        } while (pathToRoot.insert(std::make_pair(buf.st_dev, buf.st_ino)).second);
+    }
+    struct stat buf;
+    if (lstat(path.c_str(), &buf) != 0) {
+        int error = errno;
+        std::cerr << "Unable to stat " << path << ": " << strerror(error) << '\n';
+        return false;
+    }
+    if (pathToRoot.find(std::make_pair(buf.st_dev, buf.st_ino)) != pathToRoot.end()) {
+        std::cerr << "Can't remove " << path << " as it is a parent.\n";
+        return false;
+    }
+    if (contentOnly) {
+        if (!S_ISDIR(buf.st_mode)) {
+            std::cerr << path << " is not a directory.\n";
+            return false;
+        }
+    } else {
+        if (S_ISDIR(buf.st_mode)) {
+            std::pair<dev_t, ino_t> cur = std::make_pair(buf.st_dev, buf.st_ino);
+            if (cur == cwd) {
+                std::cerr << "Can't remove .\n";
+                return false;
+            }
+            std::string parent(path+"/..");
+            if (lstat(parent.c_str(), &buf) != 0) {
+                int error = errno;
+                std::cerr << "Unable to stat " << path << ": " << strerror(error) << '\n';
+                return false;
+            }
+            if (buf.st_dev != cur.first) {
+                std::cerr << "Can't remove " << path << " as it is a mount point\n";
+                return false;
+            }
+        }
+    }
+    return true;
+} // isValidForRemoval
+
+// ----------------------------------------------------------------------------
 /// The main
 int main(int argc, char* argv[])
 {
@@ -24,29 +105,43 @@ int main(int argc, char* argv[])
 
     try {
         int c, errcnt = 0;
-
+        bool force = false;
+        bool contentOnly = false;
+        
         std::locale::global(std::locale(""));
         
-        while (c = getopt(argc, argv, "h"), c != -1) {
+        while (c = getopt(argc, argv, "fch"), c != -1) {
             switch (c) {
             case 'h':
                 help();
                 throw 0;
+            case 'f':
+                force=true;
+                break;
+            case 'c':
+                contentOnly=true;
+                break;
             case '?':
-                errcnt++;
+                ++errcnt;
                 break;
             default:
                 std::cerr << "Unexpected result from getopts: " << c << '\n';
-                errcnt++;
+                ++errcnt;
             }
         }
-
-        if (optind < argc) {
+        
+        for (int i = optind; i < argc; ++i) {
+            if (!isValidForRemoval(argv[i], contentOnly))
+                ++errcnt;
+        }
+        if (optind == argc) {
+            usage();
+            ++errcnt;
         }
 
         if (errcnt > 0)
             throw EXIT_FAILURE;
-        
+
     } catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << '\n';
         status = EXIT_FAILURE;
@@ -58,4 +153,4 @@ int main(int argc, char* argv[])
     }
 
     return status;
-}
+} // main
